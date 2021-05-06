4143e7e05930f722dd173c48e66dd3ff7642ed9a
James Clark <J.Clark.NW@Gmail.com>
2021-05-06 15:36:05 -0700

    response = requests.get("https://cs361-ul-scraper.herokuapp.com/Cashew_Chicken"dda     session['ingredients'] = response.json()

--- app.py
+++ app.py
@@ -1,19 +1,43 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, session, request
 import requests
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'password'
 
-ingredients = ["tomato", "chicken", "beer"]
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    print("went home")
-    return render_template('home.html')
+    if request.method == 'POST':
+        print("POST METHOD")
+        link = request.form['wikiURL']
+        full_link = 'https://cs361-ul-scraper.herokuapp.com/' + link
+        response = requests.get(full_link)
+        session['ingredients'] = response.json()
+        return render_template('home.html', ingredients=session['ingredients'])
+    if request.method == 'GET':
+        print("GET METHOD")
+        return render_template('home.html')
 
 @app.route('/test')
 def index_test():
-    print("went to test")
-    return render_template('home.html', ingredients=ingredients)
+    response = requests.get("https://cs361-ul-scraper.herokuapp.com/Cashew_Chicken")
+    session['ingredients'] = response.json()
+    return render_template('home.html', ingredients=session['ingredients'])
+
+@app.route('/reset')
+def reset():
+    session.pop('ingredients', None)
+    session.pop('groceries', None)
+    return render_template('home.html')
+
+@app.route('/addrecipe', methods=['GET', 'POST'])
+def add_recipe():
+    if session.get('ingredients'):
+        session['groceries'] = session['ingredients']
+        session.pop('ingredients', None)
+        return render_template('home.html', groceries=session['groceries'])
+    else:
+        return render_template('home.html')
 
 
 if __name__ == "__main__":

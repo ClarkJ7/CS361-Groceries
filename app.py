27ca1f7accb5484c8556946c1eff7a65f23bddc3
James Clark <J.Clark.NW@Gmail.com>
2021-05-11 14:19:49 -0700

    added ability for recipe to be viewed after parsing

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, session, request
 import requests
+from grocerySort import sort_recipe, combine_groceries
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'password'
@@ -16,9 +17,12 @@ def index():
         full_link = 'https://cs361-ul-scraper.herokuapp.com/' + link
         # send to ulScraper API
         response = requests.get(full_link)
-        # store ingredients in session variable
-        session['ingredients'] = response.json()
-        return render_template('home.html', ingredients=session['ingredients'])
+        if response.status_code == 200:
+            # store ingredients in session variable
+            session['ingredients'] = sort_recipe(response.json())
+            return render_template('home.html', ingredients=session['ingredients'])
+        else:
+            return render_template('home.html', warning="Recipe Not Found")
     if request.method == 'GET':
         return render_template('home.html')
 
@@ -38,13 +42,18 @@ def add_recipe():
         # check if any groceries are currently shown
         if session.get('groceries'):
             # add ingredients to current groceries
-            session['groceries'].extend(session['ingredients'])
+            session['groceries'] = combine_groceries(session['ingredients'], session['groceries'])
         else:
             session['groceries'] = session['ingredients']
         session.pop('ingredients', None)
         return render_template('home.html', groceries=session['groceries'])
     else:
         return render_template('home.html')
+
+
+@app.route('/view-list')
+def view_list():
+    return render_template('viewlist.html', groceries=session['groceries'])
 
 
 if __name__ == "__main__":
